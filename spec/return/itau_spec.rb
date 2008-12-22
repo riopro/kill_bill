897ba0426791c260ef739a11221534175d684967
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2008-12-22 21:12:38 -0200

    testes para header do itau

--- spec/return/itau_spec.rb
+++ spec/return/itau_spec.rb
@@ -1,26 +1,25 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Riopro::KillBill::Return::Itau do
-  
-    before(:each) do
-      @fake_file_array = [
-        'A' * 400, # header
-        'B' * 400, # transaction 1
-        'C' * 400, # transaction 2
-        'D' * 400  # trailer
-      ]
-    end
 
     describe "Sample file test" do
       before(:each) do
         @return_file = Riopro::KillBill::Return::Itau.new('./spec/return/../../examples/sample_data/retorno_itau_teste.ret', true)
       end
       describe "header" do
+        it "should be an Itau return type" do
+          @return_file.header[:codigo_banco].should == "341"
+          @return_file.header[:nome_banco].should == "BANCO ITAU SA"
+        end
         it "should contain the agency" do
           @return_file.header[:agencia].should == "0567"
         end
         it "should contain the account" do
           @return_file.header[:conta].should == "15346"
+        end
+      end
+      describe "transaction" do
+        it "should be an Itau return type" do
         end
       end
     end
