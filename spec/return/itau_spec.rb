dc590e56582460b6c19a9e6458cd550073502598
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2008-12-22 20:55:13 -0200

    testes para Return::Itau

--- spec/return/itau_spec.rb
+++ spec/return/itau_spec.rb
@@ -10,5 +10,19 @@ describe Riopro::KillBill::Return::Itau do
         'D' * 400  # trailer
       ]
     end
+
+    describe "Sample file test" do
+      before(:each) do
+        @return_file = Riopro::KillBill::Return::Itau.new('./spec/return/../../examples/sample_data/retorno_itau_teste.ret', true)
+      end
+      describe "header" do
+        it "should contain the agency" do
+          @return_file.header[:agencia].should == "0567"
+        end
+        it "should contain the account" do
+          @return_file.header[:conta].should == "15346"
+        end
+      end
+    end
     
 end
