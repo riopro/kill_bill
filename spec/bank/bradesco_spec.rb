a06da30943e420f45c56219e0c59cf73a3cc98b4
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2009-03-04 17:37:47 -0300

    Mais testes para Bradesco, código de barras não está correto ainda. 43 caracteres ao invés de 44

--- spec/bank/bradesco_spec.rb
+++ spec/bank/bradesco_spec.rb
@@ -39,22 +39,22 @@ describe Riopro::KillBill::Bank::Bradesco do
       end
     end
   end
-#  describe "Instance methods" do
-#    before(:each) do
-#      @bank_bradesco = Riopro::KillBill::Bank::Bradesco.new
-#      @bank_bradesco.agency = "0607"
-#      @bank_bradesco.account = "15255"
-#      @bank_bradesco.our_number = "12345678"
-#    end
-#    describe "process_options" do
-#      it "should assign variables from option" do
-#        options = { :account => "0567", :our_number => "12345678", :transferor => "RIOPRO" }
-#        @bank_bradesco = Riopro::KillBill::Bank::Bradesco.new(options)
-#        @bank_bradesco.account.should == options[:account]
-#        @bank_bradesco.our_number.should == options[:our_number]
-#        @bank_bradesco.transferor.should == options[:transferor]
-#      end
-#    end
+  describe "Instance methods" do
+    before(:each) do
+      @bank_bradesco = Riopro::KillBill::Bank::Bradesco.new
+      @bank_bradesco.agency = "0607"
+      @bank_bradesco.account = "15255"
+      @bank_bradesco.our_number = "12345678"
+    end
+    describe "process_options" do
+      it "should assign variables from option" do
+        options = { :account => "0567", :our_number => "12345678", :transferor => "RIOPRO" }
+        @bank_bradesco = Riopro::KillBill::Bank::Bradesco.new(options)
+        @bank_bradesco.account.should == "0000567"
+        @bank_bradesco.our_number.should == "00012345678"
+        @bank_bradesco.transferor.should == options[:transferor]
+      end
+    end
 #    describe "pdf_parameters" do
 #      before(:each) do
 #        @bank_bradesco.due_on = Date.today
@@ -99,79 +99,66 @@ describe Riopro::KillBill::Bank::Bradesco do
 #      end
 #    end
 #
-#    describe "calculate_account_cd" do
-#      before(:each) do
-#        @bank_bradesco.agency = "1234"
-#        @bank_bradesco.account = "56789"
-#      end
-#      it "should call module10" do
-#        @bank_bradesco.should_receive(:module10).with("#{@bank_bradesco.agency}#{@bank_bradesco.account}").and_return(1)
-#        @bank_bradesco.calculate_account_cd.should == 1
-#      end
-#    end
-#
-#    describe "calculate_our_number_cd" do
-#      before(:each) do
-#        @bank_bradesco.portfolio = 175
-#      end
-#      describe "portfolio 175" do
-#        it "should call module10" do
-#          @bank_bradesco.should_receive(:module10).with("#{@bank_bradesco.agency}#{@bank_bradesco.account}#{@bank_bradesco.portfolio}#{@bank_bradesco.our_number}").and_return(1)
-#          @bank_bradesco.calculate_our_number_cd.should == 1
-#        end
-#        it "should calculate correctly" do
-#          @bank_bradesco.calculate_our_number_cd.should == 4
-#        end
-#      end
-#      describe "portfolio 126" do
-#        before(:each) do
-#          @bank_bradesco.portfolio = 126
-#        end
-#        it "should receive different with for different portfolio" do
-#          @bank_bradesco.should_receive(:module10).with("#{@bank_bradesco.portfolio}#{@bank_bradesco.our_number}").and_return(1)
-#          @bank_bradesco.calculate_our_number_cd.should == 1
-#        end
-#        it "should calculate correctly" do
-#          @bank_bradesco.calculate_our_number_cd.should == 5
-#        end
-#      end
-#
-#      describe "barcode" do
-#        before(:each) do
-#          @bank_bradesco.due_on = Date.new(2008,3,12)
-#        end
-#        it "should build barcode correctly" do
-#          @bank_bradesco.barcode.should == "34191380900000000001751234567840607152550000"
-#
-#          @bank_bradesco.value = 135.00
-#          @bank_bradesco.due_on = Date.new(2004,9,3)
-#          @bank_bradesco.agency = "0810"
-#          @bank_bradesco.account = "53678"
-#          @bank_bradesco.our_number = "258281"
-#          @bank_bradesco.barcode.should == "34195252300000135001750025828170810536789000"
-#          @bank_bradesco.calculate_barcode_cd.should == 5
-#          @bank_bradesco.build_barcode.should == "3419252300000135001750025828170810536789000"
-#          @bank_bradesco.barcode.should == "34195252300000135001750025828170810536789000"
-#
-#          @bank_bradesco.our_number = "10281"
-#          @bank_bradesco.barcode.should == "34197252300000135001750001028160810536789000"
-#        end
-#        it "should try to format our_number and value with zeros at left" do
-#          @bank_bradesco.our_number = "12345678"
-#          @bank_bradesco.value = 1.35
-#          @bank_bradesco.should_receive(:zeros_at_left).with(@bank_bradesco.value, 10).and_return("0000000135")
-#          @bank_bradesco.should_receive(:zeros_at_left).with(@bank_bradesco.our_number, 8).and_return("12345678")
-#          @bank_bradesco.barcode
-#        end
-#        it "should calculate due day factor" do
-#          @bank_bradesco.due_on.should_receive(:due_day_factor).and_return(1203)
-#          @bank_bradesco.barcode
-#        end
-#        it "should calculate account_cd" do
-#          @bank_bradesco.should_receive(:calculate_account_cd).and_return(1)
-#          @bank_bradesco.barcode
-#        end
-#      end
-#    end
-#  end
+    describe "calculate_account_cd" do
+      before(:each) do
+        @bank_bradesco.agency = "1234"
+        @bank_bradesco.account = "56789"
+      end
+      it "should call module10" do
+        @bank_bradesco.should_receive(:module10).with("#{@bank_bradesco.agency}#{@bank_bradesco.account}").and_return(1)
+        @bank_bradesco.calculate_account_cd.should == 1
+      end
+    end
+
+    describe "calculate_our_number_cd" do
+      before(:each) do
+        @bank_bradesco.portfolio = 9
+      end
+      describe "portfolio 9" do
+        it "should call module10" do
+          @bank_bradesco.should_receive(:module10).with("#{@bank_bradesco.agency}#{@bank_bradesco.account}#{@bank_bradesco.portfolio}#{@bank_bradesco.our_number}").and_return(1)
+          @bank_bradesco.calculate_our_number_cd.should == 1
+        end
+        it "should calculate correctly" do
+          @bank_bradesco.calculate_our_number_cd.should == 3
+        end
+      end
+    end
+
+    describe "barcode" do
+      before(:each) do
+        @bank_bradesco.due_on = Date.new(2008,3,12)
+      end
+      it "should build barcode correctly" do
+        @bank_bradesco.barcode.should == "34191380900000000001751234567840607152550000"
+
+        @bank_bradesco.value = 135.00
+        @bank_bradesco.due_on = Date.new(2004,9,3)
+        @bank_bradesco.agency = "0810"
+        @bank_bradesco.account = "53678"
+        @bank_bradesco.our_number = "258281"
+        @bank_bradesco.barcode.should == "34195252300000135001750025828170810536789000"
+        @bank_bradesco.calculate_barcode_cd.should == 5
+        @bank_bradesco.build_barcode.should == "3419252300000135001750025828170810536789000"
+        @bank_bradesco.barcode.should == "34195252300000135001750025828170810536789000"
+
+        @bank_bradesco.our_number = "10281"
+        @bank_bradesco.barcode.should == "34197252300000135001750001028160810536789000"
+      end
+      it "should try to format our_number and value with zeros at left" do
+        @bank_bradesco.our_number = "12345678"
+        @bank_bradesco.value = 1.35
+        @bank_bradesco.should_receive(:zeros_at_left).with(@bank_bradesco.value, 10).and_return("0000000135")
+        @bank_bradesco.barcode
+      end
+      it "should calculate due day factor" do
+        @bank_bradesco.due_on.should_receive(:due_day_factor).and_return(1203)
+        @bank_bradesco.barcode
+      end
+      it "should calculate account_cd" do
+        @bank_bradesco.should_receive(:calculate_account_cd).and_return(1)
+        @bank_bradesco.barcode
+      end
+    end
+  end
 end
